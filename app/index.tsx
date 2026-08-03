d9740f2b439be5cfad3c16b74bbe6a1c4e7d8b12
DylanBozarth <dylanbozarth@theluxergroup.com>
2026-08-03 11:31:30 -0700

    remove expo crap

---
+++ app/index.tsx
@@ -0,0 +1,34 @@
+import { StyleSheet, Text } from 'react-native';
+import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context';
+
+export default function HomeScreen() {
+  return (
+    <SafeAreaProvider>
+      <SafeAreaView style={styles.titleContainer}>
+        <Text style={styles.white}>Hello World</Text>
+      </SafeAreaView>
+    </SafeAreaProvider>
+  );
+}
+
+const styles = StyleSheet.create({
+  titleContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    gap: 8,
+  },
+  white: {
+    color: "white"
+  },
+  stepContainer: {
+    gap: 8,
+    marginBottom: 8,
+  },
+  reactLogo: {
+    height: 178,
+    width: 290,
+    bottom: 0,
+    left: 0,
+    position: 'absolute',
+  },
+});
---
+++ notes-for-author.md
@@ -0,0 +1,4 @@
+SXV Space eXploration vehicle 
+Moba style single player alien killing adventure
+Some RTS elements
+Linear missions C&C style campaign. 
